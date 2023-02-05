19e44533e5fd3872ab7d5d02342ac12dcde7b488
javi <ingenierodesoftware73@gmail.com>
2023-02-05 16:42:19 -0500

    conection to databases

--- src/lib/Databases.php
+++ src/lib/Databases.php
@@ -1,40 +1,16 @@
 <?php
 
-namespace Vidamrr\Notas\lib;
+// Datos de conexión
+$host = "172.17.0.2";
+$username = "root";
+$password = "123";
+$dbname = "notas";
 
-use mysqli;
-use PDO;
-use PDOException;
+// Crear conexión
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
-class DBConnection
-{
-    private $host = "localhost";
-    private $username = "xavi_ubuntu";
-    private $password = 123;
-    private $dbname = "notas";
-    private $conn;
-
-    public function __construct()
-    {
-        try {
-            // Crea la conexión a la base de datos
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
-            // Establece el modo de error para que lance excepciones
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexión exitosa";
-        } catch(PDOException $e) {
-            echo "Conexión fallida: " . $e->getMessage();
-        }
-    }
-
-    public function closeConnection()
-    {
-        // Cierra la conexión
-        $this->conn = null;
-    }
+// Verificar conexión
+if (!$conn) {
+    die("Conexión fallida: " . mysqli_connect_error());
 }
-
-$db = new DBConnection();
-$db->closeConnection();
-
-
+echo "Conexión exitosa";
