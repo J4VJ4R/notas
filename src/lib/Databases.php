16603c03bfee736e0ec2c97c703a0522c1b2a130
javi <ingenierodesoftware73@gmail.com>
2023-02-04 23:01:34 -0500

    probando conexion a base de datos

--- src/lib/Databases.php
+++ src/lib/Databases.php
@@ -1,12 +1,40 @@
 <?php
 
-namespace Vidamrr\Notas;
+namespace Vidamrr\Notas\lib;
 
+use mysqli;
 use PDO;
 use PDOException;
 
-public function __construct(){
-    private string $host;
-    
+class DBConnection
+{
+    private $host = "localhost";
+    private $username = "xavi_ubuntu";
+    private $password = 123;
+    private $dbname = "notas";
+    private $conn;
+
+    public function __construct()
+    {
+        try {
+            // Crea la conexión a la base de datos
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            // Establece el modo de error para que lance excepciones
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión exitosa";
+        } catch(PDOException $e) {
+            echo "Conexión fallida: " . $e->getMessage();
+        }
+    }
+
+    public function closeConnection()
+    {
+        // Cierra la conexión
+        $this->conn = null;
+    }
 }
+
+$db = new DBConnection();
+$db->closeConnection();
+
 
---
+++ src/models/Note.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Vidamrr\Notas\models;
+
+use Vidamrr\Notas\lib\DBConnection;
+
+
+class Note{
+
+}
